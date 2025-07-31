249a23363f1fbff4774272f62cebcba43e006595
Ilya New <riskpoforme@gmail.com>
2025-08-01 00:31:37 +1000

    6-3 не проходят автотесты. все работает верно, под автотсеты подкрутить не вышло, отправляю как есть

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,52 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+``
 export default class UserTable {
+  #rows = [];
+  elem = null;
+
   constructor(rows) {
+    this.#rows = rows ?? [];
+    this.#render();
+    this.#onBtnClick();
+  }
+
+  #html() {
+    return `
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${this.#rows.map(row => `
+            <tr>
+              <td>${row.name}</td>
+              <td>${row.age}</td>
+              <td>${row.salary}</td>
+              <td>${row.city}</td>
+              <td><button>[X]</button></td>
+            </tr>
+          `).join('')}
+        </tbody>
+      </table>
+    `;
+  }
+
+  #render() {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = this.#html();
+    this.elem = tmp.querySelector('table');
+  }
+
+  #onBtnClick() {
+    this.elem.addEventListener('click', (event) => {
+      if (event.target.tagName !== 'BUTTON') return;
+      const row = event.target.closest('tr');
+      if (row) row.remove();
+    });
   }
 }
